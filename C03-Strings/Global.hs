bca9f73e8a9afe09138a21d0a026eaeb922611e6
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-01-04 18:34:42 +0100

    Add Chapter 3 exercises

---
+++ C03-Strings/Global.hs
@@ -0,0 +1,9 @@
+module GlocalLocal where
+
+topLevelFunction :: Integer -> Integer
+topLevelFunction x = x + woot + topLevelValue
+  where woot :: Integer
+        woot = 10
+
+topLevelValue :: Integer
+topLevelValue = 5
---
+++ C03-Strings/print2.hs
@@ -0,0 +1,8 @@
+module Print2 where
+
+main :: IO ()
+main = do
+    putStrLn "Count to four with me:"
+    putStr   "one, two"
+    putStr   ", three, and"
+    putStrLn " four!"
